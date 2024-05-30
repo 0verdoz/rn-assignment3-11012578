7d9e27bdd11a1364244ca1dd282d1d3113c837eb
0verdoz <wmakpodo@st.ug.edu.gh>
2024-05-30 13:57:52 +0100

    Categories section completed.

--- Devs/App.js
+++ Devs/App.js
@@ -1,9 +1,21 @@
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Pressable, FlatList, TextInput } from 'react-native';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
-import { FontAwesome } from '@expo/vector-icons';
+
+const categories = [
+  { id: '1', name: 'Exercise', tasks: 12, image: require('./assets/exercise.png') },
+  { id: '2', name: 'Study', tasks: 8, image: require('./assets/study.png') },
+  { id: '3', name: 'Code', tasks: 5, image: require('./assets/code2.png') },
+  { id: '4', name: 'Cook', tasks: 7, image: require('./assets/cook3.png') },
+  { id: '5', name: 'Meditation', tasks: 8, image: require('./assets/meditation.png') },
+  { id: '6', name: 'Rest', tasks: 10, image: require('./assets/rest.png') },
+  { id: '7', name: 'Music', tasks: 5, image: require('./assets/music1.png') },
+  { id: '8', name: 'Art', tasks: 6, image: require('./assets/art.png') },
+  { id: '9', name: 'Workout', tasks: 9, image: require('./assets/workout.png') },
+  { id: '10', name: 'Travel', tasks: 6, image: require('./assets/travel.png') },
+];
 
 export default function App() {
   const [searchText, setSearchText] = useState('');
@@ -26,6 +38,16 @@ export default function App() {
         />
     );
     }
+
+    const renderCategoryItem = ({ item }) => (
+      <View style={styles.categoryCard}>
+      <Image source={item.image} style={styles.categoryImage} />
+      <View style={styles.categoryTextContainer}>
+        <Text style={styles.categoryName}>{item.name}</Text>
+        <Text style={styles.categoryTasks}>{item.tasks} Tasks</Text>
+      </View>
+    </View>
+    );
 
   return (
     <View style={styles.container}>
@@ -52,11 +74,26 @@ export default function App() {
               onChangeText={setSearchText}
           />
         </View>
+        <Pressable onPress={() => console.log('Menu button pressed.')}>
         <View style={styles.profileImageContainer}>
           <View style={styles.ellipse2} />
           <Image source={require('./assets/bx_slider.png')} style={styles.image2} />
         </View>
+        </Pressable>
       </View>
+      <View style={{height: 70}} />
+      <Text style={styles.text4}>Categories</Text>
+      <View style={{height: 20}} />
+      <View style={styles.categoryListContainer}>
+          <FlatList 
+          data={categories}
+          renderItem={renderCategoryItem}
+          keyExtractor={item => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.categoryList}/>
+        </View>
+        <View />
     </View>
   );
 }
@@ -70,6 +107,17 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#000',
   },
+  text4: {
+    fontWeight: '700',
+    fontSize: 20,
+    lineHeight: 24,
+    top: 200,
+    position: 'absolute',
+    marginTop: 20,
+    left: 20,
+    height: 24,
+    width: 98,
+  },
   text3: {
     left: 9,
     fontSize: 16,
@@ -82,7 +130,7 @@ const styles = StyleSheet.create({
     height: 52,
     position: 'absolute',
     top: 52,
-    left: 20,
+    left: 18,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -93,7 +141,7 @@ const styles = StyleSheet.create({
     height: 52,
     position: 'absolute',
     top: 134,
-    left: 20,
+    left: 16,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -145,7 +193,12 @@ const styles = StyleSheet.create({
     height: 25.5,
     position: 'absolute',
     zIndex: 2,
-    
+  },
+  searchIcon: {
+    width: 16,
+    height: 16,
+    borderWidth: 1.5,
+    tintColor: 'black',
   },
   ellipse1: {
     width: 50,
@@ -162,5 +215,46 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(240, 82, 47, 1)',
     position: 'absolute',
     zIndex: 1,
+  },
+  categoryCard: {
+    width: 186,
+    height: 192,
+    borderRadius: 15,
+    backgroundColor: 'rgba(251, 249, 247, 1)',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'relative',
+    marginRight: 15,
+  },
+  categoryImage: {
+    width: 158,
+    height: 146,
+    top: 37,
+    left: 8,
+  },
+  categoryTextContainer: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
+  categoryName: {
+    fontWeight: '700',
+    fontSize: 16,
+    color: 'black',
+  },
+  categoryTasks: {
+    fontWeight: '500',
+    fontSize: 12,
+    color: 'gray',
+  },
+  categoryListContainer: {
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    paddingHorizontal: 20,
+    top: -130,
+    justifyContent: 'space-between',
+  },
+  categoryList: {
+    paddingVertical: 20,
   },
 });
